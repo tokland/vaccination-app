a53e78f2158f38537e828fa60d1d842483722e45
Arnau Sanchez <pyarnau@gmail.com>
2019-01-22 10:16:37 +0100

    Basic landing page with 4 sections (no actions yet)

--- src/Root.component.js
+++ src/Root.component.js
@@ -1,34 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import i18n from '@dhis2/d2-i18n'
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import { withStyles } from '@material-ui/core/styles';
 
-import { withFeedback, levels } from './components/feedback'
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+});
 
 class Root extends React.Component {
     static propTypes = {
         d2: PropTypes.object.isRequired,
-        feedback: PropTypes.func.isRequired,
     }
 
-    showFeedback = () => {
-        this.props.feedback(levels.INFO, i18n.t('Hello there'))
+    onClick = (key) => {
+        console.log("TODO", "clicked", key);
     }
 
     render() {
-        const { d2 } = this.props
+        const { classes } = this.props
+        const items = [
+            ["campaign-configurator", i18n.t('Campaign Configurator')],
+            ["data-entry", i18n.t('Data Entry')],
+            ["dashboard", i18n.t('Dashboard')],
+            ["settings", i18n.t('Settings')],
+        ]
+        const menuItems = items.map(([key, title]) => (
+            <MenuItem
+                key={key}
+                data-test={`page-${key}`}
+                onClick={this.onClick.bind(this, key)}
+            >
+                {title}
+            </MenuItem>
+        ))
 
         return (
-            <React.Fragment>
-                <p>Current user: {d2.currentUser.displayName}</p>
-                
-                <p>Add your App components here</p>
-
-                <button onClick={this.showFeedback}>
-                    {i18n.t("Click to show feedback")}
-                </button>
-            </React.Fragment>
+            <div className={classes.root}>
+                <Paper>
+                    <MenuList data-test="pages">
+                        {menuItems}
+                    </MenuList>
+                </Paper>
+            </div>
         )
     }
 }
 
-export default withFeedback(Root)
+export default withStyles(styles)(Root);

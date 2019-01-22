dec747492a8e3ed943cd7548d4b12d49bef31597
Arnau Sanchez <pyarnau@gmail.com>
2019-01-22 10:17:35 +0100

    Replace puppeteer by cypress

---
+++ cypress/integration/landing-page.spec.js
@@ -0,0 +1,38 @@
+/// <reference types="Cypress" />
+
+context('Landing page', () => {
+    before(() => {});
+
+    beforeEach(() => {
+        cy.startServer('landing-page');
+        cy.login('system', 'System123');
+        cy.clock(Date.UTC(2018, 11, 16, 13, 10, 9), ['Date']);
+        cy.loadPage();
+    });
+
+    after(() => {
+        cy.saveFixtures('landing-page');
+    });
+
+    it('loads', () => {
+        cy.title().should('equal', 'Vaccination App');
+    });
+
+    it('shows 4 pages', () => {
+        cy.get('[data-test="pages"]')
+            .should('have.length', 1)
+            .should('be.visible');
+
+        cy.get('[data-test="page-campaign-configurator"]')
+            .should('have.length', 1);
+
+        cy.get('[data-test="page-data-entry"]')
+            .should('have.length', 1);
+
+        cy.get('[data-test="page-dashboard"]')
+        .should('have.length', 1);
+
+        cy.get('[data-test="page-settings"]')
+        .should('have.length', 1);
+    });
+});

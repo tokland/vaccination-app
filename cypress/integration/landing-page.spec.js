5089f840ade6b0c7939e4f79fe93041f8c122d9e
Arnau Sanchez <pyarnau@gmail.com>
2019-01-23 07:02:41 +0100

    Start server in before block

--- cypress/integration/landing-page.spec.js
+++ cypress/integration/landing-page.spec.js
@@ -1,38 +1,40 @@
-/// <reference types="Cypress" />
+/// <reference types='Cypress' />
 
 context('Landing page', () => {
-    before(() => {});
+    before(() => {
+        cy.startServer('landing-page');
+    });
 
     beforeEach(() => {
-        cy.startServer('landing-page');
-        cy.login('system', 'System123');
-        cy.clock(Date.UTC(2018, 11, 16, 13, 10, 9), ['Date']);
-        cy.loadPage();
-    });
+        cy.login('system', 'System123')
+        cy.clock(Date.UTC(2018, 11, 16, 13, 10, 9), ['Date'])
+        cy.loadPage()
+    })
 
     after(() => {
-        cy.saveFixtures('landing-page');
-    });
+        cy.saveFixtures('landing-page')
+        cy.clearCookies()
+    })
 
     it('loads', () => {
-        cy.title().should('equal', 'Vaccination App');
-    });
+        cy.title().should('equal', 'Vaccination App')
+    })
 
     it('shows 4 pages', () => {
         cy.get('[data-test="pages"]')
             .should('have.length', 1)
-            .should('be.visible');
+            .should('be.visible')
 
         cy.get('[data-test="page-campaign-configurator"]')
-            .should('have.length', 1);
+            .should('have.length', 1)
 
         cy.get('[data-test="page-data-entry"]')
-            .should('have.length', 1);
+            .should('have.length', 1)
 
         cy.get('[data-test="page-dashboard"]')
-        .should('have.length', 1);
+            .should('have.length', 1)
 
         cy.get('[data-test="page-settings"]')
-        .should('have.length', 1);
-    });
-});
+            .should('have.length', 1)
+    })
+})
